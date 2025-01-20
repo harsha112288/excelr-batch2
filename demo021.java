ba7e07b214f6f14af781c6e594e9ccfafd711e8f
harsha112288 <pavanharsha605@gmail.com>
2025-01-20 10:11:12 +0530

    name

---
+++ demo021.java
@@ -0,0 +1,10 @@
+public class demo021{
+    public static void main(String[] args) {
+        String[] arr = new String[4];
+        arr[0] = "harsha";
+        arr[1] = "pavan";
+        arr[2] = "sai";
+        arr[3] = "vedansh";
+        System.err.println(arr[3]);
+    }
+}
