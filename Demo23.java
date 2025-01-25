2b4745b69a0a8a38ba37dde309273610a8e17ddf
harsha112288 <pavanharsha605@gmail.com>
2025-01-25 09:51:03 +0530

    Add files via upload

---
+++ Demo23.java
@@ -0,0 +1,10 @@
+public class Demo23 {
+        public static void main(String[] args) {
+            for (int i = 1; i <= 100; i++) {
+                if (i != 5 && i != 7) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+    
